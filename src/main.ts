addd3cb07ff7554b20f52c16f81eba8708440968
Dag Erik <dagerikhl@gmail.com>
2018-05-08 00:43:06 +0200

    Add vuefire and firebase

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import Vue from 'vue'
+import Vue from 'vue';
+
+const VueFire = require('vuefire');
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 import 'vue-awesome/icons';
 import Icon from 'vue-awesome/components/Icon.vue';
@@ -6,6 +10,14 @@ import Icon from 'vue-awesome/components/Icon.vue';
 import App from './App.vue'
 
 Vue.config.productionTip = false;
+
+// Register plugins
+Vue.use(VueFire);
+firebase.initializeApp({
+    projectId: 'watcher-in-the-cloud',
+    databaseURL: 'https://watcher-in-the-cloud.firebaseio.com'
+});
+export const db = firebase.firestore();
 
 // Register components
 Vue.component('icon', Icon);
