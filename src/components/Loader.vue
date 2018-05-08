8068c4a926bd71c692e35cb93424ff62cc079269
Dag Erik <dagerikhl@gmail.com>
2018-05-08 02:45:47 +0200

    Add Loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="loader">
+        Loading{{dots}}
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class Loader extends Vue {
+
+        private numberOfDots = 0;
+        private dots: string = '';
+
+        created() {
+            setInterval(() => {
+                this.numberOfDots = (this.numberOfDots + 1)%4;
+                this.dots = ' .'.repeat(this.numberOfDots);
+            }, 500)
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .loader {
+        width: 200px;
+
+        margin: 0 auto;
+
+        font-size: 200%;
+    }
+</style>
