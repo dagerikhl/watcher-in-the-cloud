0066fe8ebc88b6cb3bbb3eabda85340c5048c963
Dag Erik <dagerikhl@gmail.com>
2018-05-15 23:21:32 +0200

    Rework to use env vars for firebase secrets

--- src/globals/database.ts
+++ src/globals/database.ts
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 firebase.initializeApp({
-    apiKey: 'AIzaSyDt3sxUCp1aKw0ngvH8ACt-_A0fgZQNd4w',
-    authDomain: 'watcher-in-the-cloud.firebaseapp.com',
-    databaseURL: 'https://watcher-in-the-cloud.firebaseio.com',
-    projectId: 'watcher-in-the-cloud',
-    storageBucket: 'watcher-in-the-cloud.appspot.com',
-    messagingSenderId: '927168675158'
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 });
 export const database = firebase.firestore();
 database.settings({ timestampsInSnapshots: true });
