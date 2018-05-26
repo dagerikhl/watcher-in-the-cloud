de2eae80745b402ebf4752148c8fb2ca70d0b75c
Dag Erik <dagerikhl@gmail.com>
2018-05-26 13:00:03 +0200

    Improve sorting of movies

--- src/globals/store.ts
+++ src/globals/store.ts
@@ -14,7 +14,23 @@ export const store = new Vuex.Store({
     mutations: {
         setMovies(state: any, movies: IMovies) {
             const sortedData = movies.data.sort((a, b) => {
-                return a.year === null ? 1 : b.year === null ? -1 : a.year > b.year ? 1 : a.year < b.year ? -1 : 0;
+                // First by missing year
+                if (a.year === null) return -1;
+                if (b.year === null) return 1;
+
+                // Then by newest year
+                if (a.year < b.year) return 1;
+                if (a.year > b.year) return -1;
+
+                // Then by alphabetical universe
+                if (a.universe < b.universe) return -1;
+                if (a.universe > b.universe) return 1;
+
+                // Finally by alphabetical title
+                if (a.title < b.title) return -1;
+                if (a.title > b.title) return 1;
+
+                return 0;
             });
 
             state[movies.branch.accessor] = sortedData;
@@ -24,7 +40,6 @@ export const store = new Vuex.Store({
         fetchMovies({ commit }, branch: IMovieBranch) {
             return new Promise((resolve, reject) => {
                 database.collection(branch.accessor)
-                    .orderBy('year')
                     .get()
                     .then((querySnapshot) => {
                         let data: IMovieData[] = [];
