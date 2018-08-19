f657e79b4fcd5ed0b37f23890c864141e8e250be
Dag Erik <dagerikhl@gmail.com>
2018-08-19 21:55:34 +0200

    Setup homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        DUMMY HOME
-    </div>
+    <section class="home">
+        <h1>Welcome to Watcher in the Cloud!</h1>
+        <q>The number one place to keep track of what you have seen, what you have left to see, and in many other ways
+            keep your OCD in check.</q>
+
+        <section class="login">DUMMY LOGIN</section>
+    </section>
 </template>
 
 <script lang="ts">
@@ -13,4 +17,22 @@
 </script>
 
 <style scoped lang="scss">
+    .home {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        padding: 10rem 0;
+
+        h1 {
+            margin-bottom: 2rem;
+
+            font-size: 200%;
+            font-weight: bold;
+        }
+
+        .login {
+            margin-top: 10rem;
+        }
+    }
 </style>
