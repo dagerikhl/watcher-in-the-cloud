e071e37574bc2ffad587a8a1f26bef6e04279e66
Dag Erik <dagerikhl@gmail.com>
2018-08-26 13:40:13 +0200

    Setup login component with a form with fields

--- src/containers/Login.vue
+++ src/containers/Login.vue
@@ -1,6 +1,15 @@
 <template>
-    <section class="login">
-    </section>
+    <form class="login">
+        <label for="username">E-mail address</label>
+        <input class="text-input" type="email" id="username" name="username" required autofocus
+               placeholder="Your e-mail..."/>
+
+        <label for="password">Password</label>
+        <input class="text-input" type="password" id="password" name="password" required
+               placeholder="Your password..."/>
+
+        <button class="btn" type="submit">Login</button>
+    </form>
 </template>
 
 <script lang="ts">
@@ -13,10 +22,22 @@
 
 <style scoped lang="scss">
     .login {
+        align-self: center;
         display: flex;
         flex-direction: column;
-        align-items: center;
+
+        width: 20rem;
 
         padding: 10rem 0;
+
+        label {
+            margin: 1rem 0 0.25rem 0;
+
+            font-weight: bold;
+        }
+
+        button {
+            margin-top: 2.25rem;
+        }
     }
 </style>
